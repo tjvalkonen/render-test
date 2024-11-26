9c85eb713ee245576ebaa7d15206d6fdfe59ace3
Tomas Valkonen <tomas.valkonen@helsinki.fi>
2024-11-26 07:30:59 +0200

    Update

--- index.js
+++ index.js
@@ -61,8 +61,9 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
-app.use(express.json())
+
 app.use(requestLogger)
+app.use(express.json())
 app.use(express.static('dist'))
 
 const unknownEndpoint = (request, response) => {
@@ -94,7 +95,7 @@ const generateId = () => {
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
-
+  console.log('post', body)
   if (body.content === undefined) {
     return response.status(400).json({ error: 'content missing' })
   }
@@ -131,10 +132,38 @@ app.post('/api/notes', (request, response) => {
 })
 */
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+    /*
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' })
+    })
+    */
 })
 
 /*
@@ -149,15 +178,38 @@ app.get('/api/notes/:id', (request, response) => {
   }
 })
 */
-
+/*
 app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
   notes = notes.filter(note => note.id !== id)
 
   response.status(204).end()
 })
+*/
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+// tämä tulee kaikkien muiden middlewarejen ja routejen rekisteröinnin jälkeen!
+app.use(errorHandler)
+
 
 // const PORT = process.env.PORT || 3001
 const PORT = process.env.PORT
